b69f47c02c64138f5845cbde813234cb0610fb08
Rahul Raparthi <rahulraparthi19@gmail.com>
2025-01-06 13:23:57 +0000

    Tour Service file

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/business/TourService.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/business/TourService.java
@@ -1,5 +1,8 @@
 package com.example.explorecalijpa.business;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.explorecalijpa.model.Difficulty;
@@ -27,7 +30,14 @@ public class TourService {
     return tourRepository.save(new Tour(title, description, blurb,
         price, duration, bullets, keywords, tourPackage, difficulty, region)) ;
   }
+  
+  public List<Tour> lookupByDifficulty(Difficulty difficulty) {
+    return tourRepository.findByDifficulty(difficulty);
+  }
 
+  public List<Tour> lookupByPackage(String tourPackageCode) {
+    return tourRepository.findByTourPackageCode(tourPackageCode);
+  }
   public long total() {
     return tourRepository.count();
   }
